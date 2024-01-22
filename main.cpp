0f697e251bfd04ef4fd8040bd011694a5264fa4f
niedag <105895929+niedag@users.noreply.github.com>
2024-01-22 21:13:23 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255 \n";
+}
