380cd4ec5cf9d024a23fcfa0daee68cc503e7a11
Grant Nieda <grantnieda@gmail.com>
2024-01-30 18:44:45 -0800

    changed main to add color header file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
@@ -14,15 +17,18 @@ int main() {
     for(int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width -1);
-            auto g = double(j) / (image_height -1);
-            auto b = 0;
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            
+            // Replaced 
+            // auto r = double(i) / (image_width -1);
+            // auto g = double(j) / (image_height -1);
+            // auto b = 0;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            // int ir = static_cast<int>(255.999 * r);
+            // int ig = static_cast<int>(255.999 * g);
+            // int ib = static_cast<int>(255.999 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n'; 
+            // std::cout << ir << ' ' << ig << ' ' << ib << '\n'; 
         }
     }
     std::clog << "\rDone.                 \n";
