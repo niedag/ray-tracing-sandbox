409521d7a340c3d85edfd92daf9ed828229622cf
niedag <105895929+niedag@users.noreply.github.com>
2024-03-06 00:48:14 +0000

    fixed some bugs

--- main.cpp
+++ main.cpp
@@ -56,16 +56,6 @@ int main() {
             color pixel_color = ray_color(r);
             
             write_color(std::cout, pixel_color);
-            // Replaced 
-            // auto r = double(i) / (image_width -1);
-            // auto g = double(j) / (image_height -1);
-            // auto b = 0;
-
-            // int ir = static_cast<int>(255.999 * r);
-            // int ig = static_cast<int>(255.999 * g);
-            // int ib = static_cast<int>(255.999 * b);
-
-            // std::cout << ir << ' ' << ig << ' ' << ib << '\n'; 
         }
     }
     std::clog << "\rDone.                 \n";
